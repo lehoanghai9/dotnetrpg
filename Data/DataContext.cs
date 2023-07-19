4b788e64c21a1031f297f12a94a1e0343f07d8e4
lehoanghai9 <hyperminister@gmail.com>
2023-07-19 15:57:47 +0200

    Ready for auth

---
+++ Data/DataContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace dotnetrpg.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Character> Characters => Set<Character>();
+        public DbSet<User> Users => Set<User>();
+    }
+}
---
+++ Services/CharacterService/CharacterService.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using dotnetrpg.Dtos.Character;
+
+namespace dotnetrpg.Services.CharacterService
+{
+    public class CharacterService : ICharacterService
+    {
+        private readonly IMapper _mapper;
+        private readonly DataContext _context;
+        public CharacterService(IMapper mapper, DataContext context)
+        {
+            _context = context;
+            _mapper = mapper;
+            
+        }
+        public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
+        {
+            var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
+            var character = _mapper.Map<Character>(newCharacter);
+
+            _context.Characters.Add(character);
+            await _context.SaveChangesAsync();
+            serviceResponse.Data = await _context.Characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToListAsync();
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<List<GetCharacterDto>>> DeleteCharacter(int id)
+        {
+            ServiceResponse<List<GetCharacterDto>> response = new ServiceResponse<List<GetCharacterDto>>();
+
+            try
+            {
+            var character = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
+            if(character is null)
+                throw new Exception($"Character with Id '{id}' not found.");
+
+            _context.Characters.Remove(character);
+            await _context.SaveChangesAsync();
+
+            response.Data = await _context.Characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToListAsync() ;
+            } 
+            catch(Exception ex)
+            {
+                response.Success = false;
+                response.Message = ex.Message;
+            }
+            
+
+            return response;
+        }
+
+        public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
+        {
+            var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
+            var dbCharacters = await _context.Characters.ToListAsync();
+            serviceResponse.Data = dbCharacters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+            return serviceResponse;
+
+        }
+
+        public async Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id)
+        {
+            var serviceResponse = new ServiceResponse<GetCharacterDto>();
+            var character = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
+            serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updatedCharacter)
+        {
+            ServiceResponse<GetCharacterDto> response = new ServiceResponse<GetCharacterDto>();
+
+            try
+            {
+            var character = await _context.Characters.FirstOrDefaultAsync(c => c.Id == updatedCharacter.Id);
+
+            _mapper.Map(updatedCharacter, character);
+
+            await _context.SaveChangesAsync();
+
+            response.Data = _mapper.Map<GetCharacterDto>(character);
+            } catch(Exception ex)
+            {
+                response.Success = false;
+                response.Message = ex.Message;
+            }
+            
+
+            return response;
+        }
+    }
+}
